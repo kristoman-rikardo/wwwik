fd418996d081c68676c5fa1157e710b191b9be53
Kristoffer Strømdal Wik <kristofferswik@gmail.com>
2026-05-25 12:22:02 +0200

    deplyendringer

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Gå til produksjonsmappen
 cd /usr/share/nginx/html/
-# Dra ned den nyeste koden
+# 1. Ta vare på eventuelle endringer du har gjort direkte på serveren
+git stash
+
+# 2. Hent den nye koden fra GitHub
 git pull
+
+# 3. Pop endringene dine tilbake på toppen av den nye koden
+git stash pop
